10c9aab5ade9e9b4f9a3779285a2a44acf960cd1
Scillae <2281170161@qq.com>
2021-09-15 00:09:59 -0700

    confirmmed summ_plot.py, applying to others

--- plot_tasks/summ_tasks/summ_plot_as.py
+++ plot_tasks/summ_tasks/summ_plot_as.py
@@ -1,10 +1,6 @@
-from readers import Reader, NanoConstructor
-from calc_tasks import patch_angle_calc, k2_calc, x20_star, arm_stiffness_calc
-from plot_tasks.ns_plots import ns_as_plot as ns_as_plot
-from utils.tools import save_load, chkdir
-from utils.summ_plot import summ_plot
-from collections import OrderedDict
-import matplotlib
+from utils.tools import chkdir
+from utils.summ_plot import summ_plot, SL
+from plot_tasks.ns_plots.ns_as_plot import ns_as_plot
 import numpy as np
 import os.path
 
@@ -19,18 +15,7 @@ def summ_plot_as(conf_suffix, dims_ls, conc_list, temp_list, arm_num_list, task_
     params = (xlim, ylim_avg, ylim_std, ylim_skw)
     #### conf ends ####
     #### SL ####
-    savepath = f'summary/{arm_num_list}Arms{conf_suffix}/{temp_list}C-{conc_list}M'
-    su_path = f'{savepath}-as.sudic' # (p_angs_dic, ns_tm, ns_last, sys)
-    su_dic_results = save_load(su_path, None)
-    if su_dic_results == False:
-        summary_dic = {} # {(keys):(mn)}
-        for arm_num in arm_num_list:
-            for conc in conc_list:
-                for temp in temp_list:
-                    m1, std, m3_s = ns_as_plot(single=True, arms=arm_num, temp=temp, conc=conc, conf_suffix=conf_suffix, dims_ls=dims_ls)
-                    summary_dic[(arm_num,conc,temp)] = (m1,std,m3_s)
-        su_dic_results = save_load(su_path, summary_dic)      
-    summary_dic = su_dic_results
+    summary_dic, savepath = SL(ns_as_plot ,conf_suffix, dims_ls, conc_list, temp_list, arm_num_list)
     #### SL ends ####
     plt = summ_plot(summary_dic, params, conf_suffix, dims_ls, conc_list, temp_list, arm_num_list, task_list, color_list, marker_list)
     # special tasks
--- utils/summ_plot.py
+++ utils/summ_plot.py
@@ -1,12 +1,24 @@
-from readers import Reader, NanoConstructor
-from calc_tasks import patch_angle_calc, k2_calc, x20_star, arm_stiffness_calc
-from plot_tasks.ns_plots import ns_as_plot as ns_as_plot
-from utils.tools import save_load, chkdir
-from collections import OrderedDict
+from utils.tools import save_load
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
-import os.path
+
+
+def SL(ns_func, conf_suffix, dims_ls, conc_list, temp_list, arm_num_list):
+    savepath = f'summary/{arm_num_list}Arms{conf_suffix}/{temp_list}C-{conc_list}M'
+    su_path = f'{savepath}-as.sudic' # (p_angs_dic, ns_tm, ns_last, sys)
+    su_dic_results = save_load(su_path, None)
+    if su_dic_results == False:
+        summary_dic = {} # {(keys):(mn)}
+        for arm_num in arm_num_list:
+            for conc in conc_list:
+                for temp in temp_list:
+                    m1, std, m3_s = ns_func(single=True, arms=arm_num, temp=temp, conc=conc, conf_suffix=conf_suffix, dims_ls=dims_ls)
+                    summary_dic[(arm_num,conc,temp)] = (m1,std,m3_s)
+        su_dic_results = save_load(su_path, summary_dic)      
+    return su_dic_results, savepath
+
+
 
 def summ_plot(summary_dic, plot_confs, conf_suffix, dims_ls, conc_list, temp_list, arm_num_list, task_list, color_list, marker_list):
     xlim, ylim_avg, ylim_std, ylim_skw = plot_confs # unpack configurations
